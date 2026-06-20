0bbf4ef31a970f138108ed6644583604527e1ac6
EAI <vis@emmauscollege.nl>
2026-06-21 01:14:32 +0200

    Export runtime package API

--- eatme/__init__.py
+++ eatme/__init__.py
@@ -1,6 +1,16 @@
 from .engine import EATRuntimeGatekeeper
 from .evaluator import GatekeeperOrchestrator
 from .models import Decision, GatekeeperConfig, Mode
+from .tracing import TraceAggregator, TraceLogger
 from .validator import EATValidator
 
-__all__ = ["EATRuntimeGatekeeper", "GatekeeperOrchestrator", "Mode", "Decision", "GatekeeperConfig", "EATValidator"]
+__all__ = [
+    "EATRuntimeGatekeeper",
+    "GatekeeperOrchestrator",
+    "Mode",
+    "Decision",
+    "GatekeeperConfig",
+    "TraceAggregator",
+    "TraceLogger",
+    "EATValidator",
+]
